088a57b0f0e2ff31c405911d38fdbb44582fb326
unknown <bishtchandan023@gmail.com>
2023-03-08 15:10:29 +0530

    Updated products section

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,40 +1,95 @@
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import data from '../constants/data';
+import {
+  Favorite,
+  FavoriteBorder,
+  FilterListOutlined,
+  Search,
+} from "@mui/icons-material";
+import React, { useState } from "react";
+import data from "../constants/data";
 
 function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 const Products = () => {
+  const [isActive, setIsActive] = useState(null);
+  const [like, setLike] = useState(false);
+  const [hover, setHover] = useState(false);
+  const productsoverview = data[0].home[0].products;
+  const links = productsoverview[0].links;
+  const allproducts = productsoverview[0].all;
 
-    const [isActive, setIsActive] = useState(false);
-    const productsoverview = data[0].home[0].products;
-    const links = productsoverview[0].links;
-
-    const handleOnClick = event => {
-        setIsActive(current => !current);
-    }
   return (
-    <section className='px-20 py-5 m-auto max-w-[90rem]'>
-        <h3 className='text-2xl font-medium text-gray-800 uppercase'>Product Overview</h3>
-        <div className='flex items-center gap-10 py-8'>
-        {links.map((link, index) => (
-            <NavLink 
-                key={link.id}
-                to={link.href}
-                className={classNames(
-                    "text-gray-500 text-sm hover:underline hover:text-gray-900", 
-                    isActive? "text-gray-900 underline": ""
-                )}
-                onClick={handleOnClick}
+    <section className="px-20 py-5 m-auto max-w-[90rem]">
+      <h3 className="text-2xl font-medium text-gray-800 uppercase">
+        Product Overview
+      </h3>
+      <div className="flex items-center justify-between py-8">
+        <div className="flex items-center gap-10">
+          {links.map((link, index) => (
+            <button
+              key={index}
+              className={classNames(
+                " text-md text-gray-500 hover:underline hover:text-gray-900",
+                link === isActive ? "text-gray-900 underline" : null
+              )}
+              onClick={() => setIsActive(link)}
             >
-                {link.link_title}
-            </NavLink>
-        ))}
+              {link.link_title}
+            </button>
+          ))}
         </div>
+        <div className="flex items-center gap-2">
+          <button className="px-6 py-2 text-center text-gray-500 cursor-pointer border border-gray-200 rounded hover:bg-violet-400 hover:text-white">
+            <FilterListOutlined className="w-6 h-6 mr-2 hover:text-white" />
+            Filter
+          </button>
+          <button className="px-6 py-2 text-center text-gray-500 cursor-pointer border border-gray-200 rounded hover:bg-violet-400 hover:text-white">
+            <Search className="w-6 h-6 mr-2 hover:text-white" />
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="py-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 place-items-baseline">
+          {allproducts.map((product, index) => (
+            <div className="col-span-1" key={product.id}>
+              <div className="relative group h-full">
+                <img
+                  src={product.image}
+                  alt={product.title}
+                  className="object-contain w-60 h-72"
+                />
+                <div className="hidden group-hover:flex justify-center absolute inset-x-0 bottom-8 transform translate-y-100 duration-700 ">
+                  <button className="px-8 py-2 bg-white text-gray-800 hover:bg-black hover:text-white rounded-full duration-500 ease">
+                    Quick View
+                  </button>
+                </div>
+              </div>
+              <div className="flex items-center justify-between pt-6">
+                <p className="text-gray-500 text-sm">{product.title}</p>
+                <div
+                  onClick={() => setLike((prevLike) => !prevLike)}
+                  onMouseEnter={() => setHover(true)}
+                  onMouseLeave={() => setHover(false)}
+                  key={product.id}
+                >
+                  {like || hover ? (
+                    <Favorite className="text-violet-400 cursor-pointer" />
+                  ) : (
+                    <FavoriteBorder className="text-gray-500 cursor-pointer" />
+                  )}
+                </div>
+              </div>
+              <p className="text-gray-800 font-medium text-base">
+                &#8377; {product.price}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,39 +1,63 @@
-import React, { useState } from 'react'
-import { ArrowBackIosNewOutlined, ArrowForwardIosOutlined } from '@mui/icons-material';
+import React, { useEffect, useState } from "react";
+import {
+  ArrowBackIosNewOutlined,
+  ArrowForwardIosOutlined,
+} from "@mui/icons-material";
 
-const Slider = ({items}) => {
-
+const Slider = ({ items }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
-    const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide? items.length - 1 : currentIndex - 1;
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? items.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
-  }
+  };
 
   const nextSlide = () => {
     const isLastSlide = currentIndex === items.length - 1;
     const newIndex = isLastSlide ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex)
-  }
+    setCurrentIndex(newIndex);
+  };
+
+  useEffect(() => {
+    setTimeout(
+      () =>
+        setCurrentIndex((prevIndex) =>
+          prevIndex === items.length - 1 ? 0 : prevIndex + 1
+        ),
+      6000
+    );
+    return () => {};
+  }, [currentIndex]);
 
   // const goToSlide = slideIndex => {
   //   setCurrentIndex(slideIndex)
   // }
 
   return (
-    <div className='relative h-full group'>
-      <div className='hidden group-hover:block absolute top-1/2 -translate-y-2/4 left-8 text-2xl text-black z-10 cursor-pointer'>
+    <div className="relative h-full group">
+      <div className="hidden group-hover:block absolute top-1/2 -translate-y-2/4 left-8 text-2xl text-black z-10 cursor-pointer">
         <ArrowBackIosNewOutlined onClick={prevSlide} />
       </div>
-      <div className='hidden group-hover:block absolute top-1/2 -translate-y-2/4 right-8 text-2xl text-black z-10 cursor-pointer'>
+      <div className="hidden group-hover:block absolute top-1/2 -translate-y-2/4 right-8 text-2xl text-black z-10 cursor-pointer">
         <ArrowForwardIosOutlined onClick={nextSlide} />
       </div>
-      <div style={{ backgroundImage: `url(${items[currentIndex].img})`}} className="w-full h-full bg-cover">
-        <div className='absolute inset-y-0 left-[15%] transform translate-y-2/4'>
-          <p className='text-2xl font-medium text-gray-500 animate-slideintop delay-1000'>{items[currentIndex].title}</p>
-          <h2 className='text-6xl font-bold text-gray-800 pt-3 pb-6 animate-slideinright delay-1000'>{items[currentIndex].description}</h2>
-          <button className='bg-violet-400 text-white animate-slideinbottom delay-1000 uppercase font-semibold px-10 py-3 rounded-full hover:bg-black'>Shop Now</button>
+      <div
+        style={{
+          backgroundImage: `url(${items[currentIndex].img})`,
+        }}
+        className="w-full h-full bg-cover"
+      >
+        <div className="absolute inset-y-0 left-[15%] transform translate-y-2/4">
+          <p className="text-2xl font-medium text-gray-500 animate-slideintop delay-1000">
+            {items[currentIndex].title}
+          </p>
+          <h2 className="text-6xl font-bold text-gray-800 pt-3 pb-6 animate-slideinright delay-1000">
+            {items[currentIndex].description}
+          </h2>
+          <button className="bg-violet-400 text-white animate-slideinbottom uppercase font-semibold px-10 py-3 rounded-full hover:bg-black">
+            Shop Now
+          </button>
         </div>
       </div>
       {/* <div className='flex justify-center'>
@@ -42,7 +66,7 @@ const Slider = ({items}) => {
         ))}
       </div> */}
     </div>
-  )
-  }
-  
-  export default Slider;
+  );
+};
+
+export default Slider;
